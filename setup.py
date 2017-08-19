c207827e1aa84d82ee909e2c9f35ff1be6b738c3
kkshivnarain <kkshivnarain@gmail.com>
2017-08-19 09:56:41 +0530

    Initial Files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'WorkHours',
+  packages = ['WorkHours'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Time difference calculation in business hours',
+  author = 'KK',
+  author_email = 'kkshivnarain@gmail.com',
+  url = 'https://github.com/kkshivnarain/WorkHours', # use the URL to the github repo
+  download_url = 'https://github.com/kkshivnarain/WorkHours/', # I'll explain this in a second
+  keywords = ['Business Hours', 'Time Difference'], # arbitrary keywords
+  classifiers = [],
+)
