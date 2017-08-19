85368f2a2ab2edef89cd1573de911f5caddded50
kkshivnarain <kkshivnarain@gmail.com>
2017-08-19 10:43:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'KK',
   author_email = 'kkshivnarain@gmail.com',
   url = 'https://github.com/kkshivnarain/WorkHours', # use the URL to the github repo
-  download_url = 'https://github.com/kkshivnarain/WorkHours/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/kkshivnarain/WorkHours/archive/0.2.tar.gz', # I'll explain this in a second
   keywords = ['Business Hours', 'Time Difference'], # arbitrary keywords
   classifiers = [],
 )
